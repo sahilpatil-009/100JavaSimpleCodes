469bdbb6cfee06f1f89a965df9a1bbfad39c3659
Sahil Patil <122969868+sahilpatil-009@users.noreply.github.com>
2024-02-24 22:45:48 +0530

    Add files via upload

---
+++ 35_Decoding.java
@@ -0,0 +1,19 @@
+/*Java Code to Count Possible Decoding Of A Given Digit Sequence
+
+Count Possible Decoding Of A Given Digit Sequence in Java
+Here we will discuss how to count all the possible decoding of a given digit sequence in Java. Before counting the number of decodings first let’s see how the numbers are assumed to be coded. The decoding programs are the most possible questions asked and are largely practiced in Java programming. The program counts the number of possible decoding’s of the entered digit by the user of the given sequence.
+
+Explanation
+1 = A, 2 = B, 3 = C, . . . . , 26 = Z.
+The decoding programs are the most possible questions asked and are largely practiced in C  programming. The program counts the number of possible decoding’s of the entered digit by the user of the given sequence.
+For example :- if the digit sequence is “12” then there are two possible decoding’s for this – One of them is ‘AB’ when we decode 1 as ‘A’ and 2 as ‘B’. Now we can also decode this digit sequence “12” as ‘L’ when we decode digits 1 and 2 taken together as an integer 12.
+
+Now let’s take another example for reference,
+
+Sequence = 131
+
+Possible decoding (1, 3, 1,)    = ACA
+Possible decoding (13, 1)   = MA
+So, the total possible decoding’s of sequence 131 is 2.
+
+*/
